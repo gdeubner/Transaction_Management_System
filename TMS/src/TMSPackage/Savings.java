01b7d52d150c50d3b865feffce87f0a0b66e0de8
Sandeep Alankar <61889822+sandeepalankar@users.noreply.github.com>
2020-10-02 21:31:27 -0400

    Finished code, need to finish comments

--- TMS/src/TMSPackage/Savings.java
+++ TMS/src/TMSPackage/Savings.java
@@ -9,21 +9,44 @@ package TMSPackage;
  */
 public class Savings extends Account{
     private boolean isLoyal;
-    
+
+    /**
+     * 
+     * @param holder
+     * @param balance
+     * @param dateOpen
+     * @param isLoyal
+     */
     public Savings(Profile holder, double balance, Date dateOpen, boolean isLoyal) {
         super(holder, balance, dateOpen);
         this.isLoyal = isLoyal;
     }
 
+    /**
+     * 
+     */
     @Override
     public double monthlyInterest() {
-        // TODO Auto-generated method stub
-        return 0;
+        double monthInterest = 0.0025; //annual interest rate of 0.25%
+        double isLoyalInterest = 0.0035; //loyal customer interest rate of 0.35%
+        if (isLoyal) {
+            return isLoyalInterest/Month.TOTALMONTHS;
+        }
+        else {
+            return monthInterest/Month.TOTALMONTHS;
+        }
     }
 
+    /**
+     * 
+     */
     @Override
     public double monthlyFee() {
-        // TODO Auto-generated method stub
-        return 0;
+        if (super.getBalance() >= 300) {
+            return 0;
+        }
+        else {
+            return 5;
+        }       
     }
 }
