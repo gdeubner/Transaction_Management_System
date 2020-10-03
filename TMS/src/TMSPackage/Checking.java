5ba35857f018d25b1663b144fb7c9fb0ebe7aab1
Sandeep Alankar <61889822+sandeepalankar@users.noreply.github.com>
2020-10-02 21:30:08 -0400

    Finished code, need to finish comments

--- TMS/src/TMSPackage/Checking.java
+++ TMS/src/TMSPackage/Checking.java
@@ -7,23 +7,33 @@ package TMSPackage;
  * @author gdeub
  *
  */
-public class Checking extends Account{
+public class Checking extends Account {
     private boolean directDeposit;
-    
+
     public Checking(Profile holder, double balance, Date dateOpen, boolean directDeposit) {
         super(holder, balance, dateOpen);
         this.directDeposit = directDeposit;
     }
-
+    
+    /**
+     * 
+     */
     @Override
-    public double monthlyInterest() {
-        // TODO Auto-generated method stub
-        return 0;
+    public double monthlyInterest() {  
+        double monthInterest = 0.0005/12;
+        return monthInterest;
     }
 
+    /**
+     * 
+     */
     @Override
-    public double monthlyFee() {
-        // TODO Auto-generated method stub
-        return 0;
+    public double monthlyFee() {        
+        if (directDeposit || super.getBalance() >= 1500) {
+            return 0;
+        } 
+        else {
+            return 25;
+        }  
     }
 }
