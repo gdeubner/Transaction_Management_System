70b79fb7d31455770899713a762b4e05022e6e02
[Graham Deubner] <[gdeubner@gmail.com]>
2020-10-05 10:33:34 -0400

    First attempt at implementing quicksort. need database class to test it.

---
+++ TMS/src/TMSPackage/ComparableObject.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package TMSPackage;
+
+/**
+ * @author gdeub
+ *
+ */
+public abstract class ComparableObject {
+    
+    public abstract <T extends Comparable<T>> T getCombarableObject(Account acc);
+
+}
