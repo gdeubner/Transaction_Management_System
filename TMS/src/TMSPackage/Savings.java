9d04d3e57420f2590d9c572f36368b01363a12f7
Sandeep Alankar <61889822+sandeepalankar@users.noreply.github.com>
2020-10-05 02:07:57 -0400

    Finished code + comments

--- TMS/src/TMSPackage/Savings.java
+++ TMS/src/TMSPackage/Savings.java
@@ -4,18 +4,22 @@
 package TMSPackage;
 
 /**
- * @author gdeub
- *
+ * This class defines a savings account and its unique features.
+ * It implements the abstract methods from Account, adding the savings account
+ * options along with its monthly fee and interest rate. * 
+ * @author Sandeep Alankar, Graham Deubner
  */
 public class Savings extends Account{
     private boolean isLoyal;
 
     /**
+     * Parameterized constructor that calls the constructor from the superclass
+     * and adds a fourth parameter, isLoyal, which is unique to a savings account.
      * 
-     * @param holder
-     * @param balance
-     * @param dateOpen
-     * @param isLoyal
+     * @param holder - name of person owning account
+     * @param balance -  money amount in account
+     * @param dateOpen -  date that account was opened
+     * @param isLoyal - boolean that is true if customer is loyal, false otherwise
      */
     public Savings(Profile holder, double balance, Date dateOpen, boolean isLoyal) {
         super(holder, balance, dateOpen);
@@ -23,7 +27,10 @@ public class Savings extends Account{
     }
 
     /**
+     * This method returns the monthly interest rate, which is different depending on whether
+     * or not the customer is loyal or not.
      * 
+     * @return monthly interest rate, which is annual interest date divided by num of months
      */
     @Override
     public double monthlyInterest() {
@@ -38,7 +45,10 @@ public class Savings extends Account{
     }
 
     /**
+     * This method returns the monthly fee, which depends on the current balance in the 
+     * account.
      * 
+     * @return monthly fee that holder pays
      */
     @Override
     public double monthlyFee() {
@@ -48,5 +58,20 @@ public class Savings extends Account{
         else {
             return 5;
         }       
+    }
+    
+    /*
+     * This method checks if the customer is loyal, and if so, marks the acocunt as special savings.
+     * 
+     * @return special savings account if the holder is loyal, empty string otherwise
+     */
+    @Override
+    public String getSpecialString() {
+        if (isLoyal) {
+            return "special Savings account";
+        }
+        else {
+            return "";
+        }
     }
 }
