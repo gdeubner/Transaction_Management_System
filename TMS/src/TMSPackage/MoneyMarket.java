50d1fcb4db84c0904950585396bfa4a08a0326d8
Sandeep Alankar <61889822+sandeepalankar@users.noreply.github.com>
2020-10-07 16:15:36 -0400

    Fixed monthlyInterest()

--- TMS/src/TMSPackage/MoneyMarket.java
+++ TMS/src/TMSPackage/MoneyMarket.java
@@ -4,94 +4,94 @@
 package TMSPackage;
 
 /**
- * This class defines a MoneyMarket account and its unique features.
- * It implements the abstract methods from Account, adding the MoneyMarket account
+ * This class defines a savings account and its unique features.
+ * It implements the abstract methods from Account, adding the savings account
  * options along with its monthly fee and interest rate. * 
  * @author Sandeep Alankar, Graham Deubner
  */
-public class MoneyMarket extends Account{   
-    private int withdrawals;
+public class Savings extends Account{
+    private boolean isLoyal;
 
     /**
      * Parameterized constructor that calls the constructor from the superclass
-     * and adds a fourth parameter, withdrawals, which is unique to a MoneyMarket account.
+     * and adds a fourth parameter, isLoyal, which is unique to a savings account.
      * 
      * @param holder - name of person owning account
      * @param balance -  money amount in account
      * @param dateOpen -  date that account was opened
+     * @param isLoyal - boolean that is true if customer is loyal, false otherwise
      */
-    public MoneyMarket(Profile holder, double balance, Date dateOpen) {
+    public Savings(Profile holder, double balance, Date dateOpen, boolean isLoyal) {
         super(holder, balance, dateOpen);
-        withdrawals = 0;
+        this.isLoyal = isLoyal;
     }
 
     /**
-     * This method returns the monthly interest, which is the MoneyMarket annual interest
-     * rate divided by the total number of months in a year times the account balance.
+     * This method returns the monthly interest amount, which is different depending on whether
+     * or not the customer is loyal or not.
      * 
-     * @return monthly interest amount
+     * @return monthly interest amount, monthly interest rate times account balance
      */
     @Override
     public double monthlyInterest() {
-        double interestRate = 0.0065;
-        return interestRate/Month.TOTALMONTHS * super.getBalance();    
+        double monthInterest = 0.0025; //annual interest rate of 0.25%
+        double isLoyalInterest = 0.0035; //loyal customer interest rate of 0.35%
+        if (isLoyal) {
+            return isLoyalInterest/Month.TOTALMONTHS * super.getBalance();
+        }
+        else {
+            return monthInterest/Month.TOTALMONTHS * super.getBalance();
+        }
     }
 
     /**
-     * This method returns the monthly fee that the customer pays on their MoneyMarket
-     * account, which is dependent on the total number of withdrawals and the current balance.
+     * This method returns the monthly fee, which depends on the current balance in the 
+     * account.
      * 
-     * @return monthly fee that holder must pay
+     * @return monthly fee that holder pays
      */
     @Override
     public double monthlyFee() {
-        if (super.getBalance() >= 2500 && withdrawals <= 6) {
+        if (super.getBalance() >= 300) {
             return 0;
-        }  
-        else {
-            return 12;
         }
+        else {
+            return 5;
+        }       
     }
-
+    
     /**
-     * toString method that calls superclass toString method and adds on number of 
-     * withdrawals.
+     * toString that calls superclass toString method and adds on account type and special savings feature
+     * if necessary.
      * 
-     * @return String representation of account type, superclass toString, and withdrawal number
+     * @return String representation of account type, superclass toString, and special savings
      */
     @Override
     public String toString() {
-        return "*Money Market*" + super.toString() + getSpecialString();
-    }
-
+        return "*Savings*" + super.toString() + getSpecialString();
+    }    
+    
     /*
-     * This method returns the number of withdrawals the holder has made on the account.
+     * This method checks if the customer is loyal, and if so, marks the acocunt as special savings.
      * 
-     * @return number of withdrawals followed by either "withdrawal" or "withdrawals" 
+     * @return special savings account if the holder is loyal, empty string otherwise
      */
     @Override
     public String getSpecialString() {
-        if (withdrawals == 1) {
-            return "*" + withdrawals + " withdrawal*";
+        if (isLoyal) {
+            return "*special Savings account*";
         }
         else {
-            return "*" + withdrawals + " withdrawals*";
+            return "";
         }
     }
-
+    
     /**
-     * returns 'm' indicating this Account is a MoneyMarket account
-     * @return returns the char 'm'
+     * returns 's' indicating this Account is a savings account
+     * @return returns the char 's'
      */
     @Override
     public char getAccountType() {
-        return 'm';
-    }
-
-    /**
-     * This method increments withdrawals
-     */
-    public void incrementWithdrawal() {
-        withdrawals++;
+        return 's';
     }    
 }
